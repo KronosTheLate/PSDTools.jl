327daa5c8ea7155368553eb5a3a01603785fcbf3
KronosTheLate <densbal@live.no>
2023-11-16 15:23:53 +0100

    Cp

--- src/data_collection_zmq.jl
+++ src/data_collection_zmq.jl
@@ -42,30 +42,3 @@ function kernel_read_data!(voltages_channel, sockets,
     end
 end
 export kernel_read_data!
-
-"""
-    kernel_read_data!(voltages_channel, sockets, reciever_buffers)
-"""
-function kernel_read_data_dennis!(voltages_channel, socket,
-    reciever_buffers, T_voltages_sent, read_request_size
-)
-    # Hoping and praying that we are reading from each socket 
-    # at the same time, and able to capture all incoming data.
-    @sync begin
-        Threads.@spawn reciever_buffers[1] .= recv(sockets[1], Vector{T_voltages_sent})
-        Threads.@spawn reciever_buffers[2] .= recv(sockets[2], Vector{T_voltages_sent})
-        Threads.@spawn reciever_buffers[3] .= recv(sockets[3], Vector{T_voltages_sent})
-        Threads.@spawn reciever_buffers[4] .= recv(sockets[4], Vector{T_voltages_sent})
-    end
-
-    for i in 1:read_request_size
-        measurement_set = (
-            reciever_buffers[2][i],
-            reciever_buffers[1][i],
-            reciever_buffers[3][i],
-            reciever_buffers[4][i]
-        )
-        put!(voltages_channel, measurement_set)
-    end
-end
-export kernel_read_data!
